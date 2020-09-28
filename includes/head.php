ff4fdc2cffbb4b74b95bc5815b518be5d6aa2ac9
José Aguilera <joseaguilera1709@gmail.com>
2020-09-28 09:25:32 -0400

    Template de Vistas

---
+++ includes/head.php
@@ -0,0 +1,26 @@
+<head>
+	<title>EDT - PY</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="EDT - Py Tienda">
+	<meta name="keywords" content="edt, eCommerce, tienda, ropas">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<header class="header-section">
+		<div class="container-fluid">
+			<!-- logo -->
+			<div class="site-logo">
+				<img src="img/logo.png" alt="logo">
+			</div>
+			<!-- responsive -->
+			<div class="nav-switch">
+				<i class="fa fa-bars"></i>
+			</div>
+			<div class="header-right">
+				<a href="cart.php" class="card-bag"><img src="img/icons/bag.png" alt=""><span>2</span></a>
+				<a href="#" class="search"><img src="img/icons/search.png" alt=""></a>
+			</div>
+			<!-- site menu -->
+			<ul class="main-menu">
+				<li><a href="index.php">Inicio</a></li>
+				<li><a href="#">Femenino</a></li>
+				<li><a href="#">Masculino</a></li>
+				<li><a href="#">Novedades</a></li>
+				<li><a href="contact.php">Contacto</a></li>
+			</ul>
+		</div>
+	</header>
